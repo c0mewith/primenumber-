e18dd0d5da7ec8414fe7273feb16451b32bc5836
c0mewith <sangjjang1004@gmail.com>
2021-02-27 16:35:40 +0900

    add code <primenumber.cpp>

---
+++ primenumber.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+	int i = 2, j = 0;
+	int num;
+	int n = 0;
+	printf("출력할 소수의 개수 : ");
+	scanf_s("%d", &num);
+	printf("\n");
+
+	for (i = 2; n < num; i++)
+	{
+		for (j = 2; j <= i; j++)
+		{
+			if (i % j == 0 && j != i)
+				break;
+			else if (i == j)
+			{
+				printf("%d ", j);
+				n += 1;
+			}
+			else
+				continue;
+		}
+	}
+	return 0;
+}
+
+		
+	
+
